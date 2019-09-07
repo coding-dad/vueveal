17b5caf81c41c084556cbda91d45ffa0a3fec2ae
coding-dad <boris.365@outlook.com>
2019-09-07 09:47:14 +0200

    added two column element

---
+++ src/components/RCols.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="container">
+		<div class="col">
+			<slot name="left" />
+		</div>
+		<div class="col">
+			<slot name="right" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "r-cols"
+};
+</script>
+
+<style scoped>
+	.container {
+		display: flex;
+	}
+	.col {
+		flex: 1;
+	}
+</style>
