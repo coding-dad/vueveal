65e2f87accbf4d74f3ab65ee089393382d50e815
coding-dad <boris.365@outlook.com>
2019-09-07 09:47:43 +0200

    added section with props (incomplete)

---
+++ src/components/RSection.vue
@@ -0,0 +1,38 @@
+<template>
+	<section :data-markdown="markdown" :data-background-image="bgImage" :data-background-size="bgSize">
+		<div v-if="content">{{ content }}</div>
+		<slot></slot>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "r-section",
+	props: {
+		markdown: {
+			type: Boolean,
+			default: false
+		},
+		bgImage: {
+			type: String,
+			default: null
+		},
+		bgSize: {
+			type: String,
+			default: null
+		}
+	},
+
+	data: () => ({ content: null }),
+
+	mounted() {
+		// If length === 1, we don't have tags inside, so we present text only
+		// Otherwise we render the slot data only to don't get the text twice
+		this.content =
+			this.$slots.default.length === 1 && this.$slots.default[0].text;
+	}
+};
+</script>
+
+<style>
+</style>
